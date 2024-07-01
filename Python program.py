1fcf85ca40ce60f945fe6072d2a5b91480828b09
Namthuannguyen <thuannguyen21042002@gmail.com>
2024-07-01 10:20:08 +0700

    fourth commit

--- Python program.py
+++ Python program.py
@@ -2,3 +2,7 @@ print('Hello world!, this is the first time I am here')
 name = 'Thuan'
 age = 20
 print(f'My name is {name}, I am {age} years old')
+
+address = 'Binh Duong'
+
+print(f'My address is {address}')
